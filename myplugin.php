0c806b9fb9b0b9055599cd959c88c5c01f6e2763
Neeraj Malwal <neerajm@bsf.io>
2024-02-26 14:19:39 +0530

    registering a meta key for a custom post type example

--- myplugin.php
+++ myplugin.php
@@ -37,3 +37,70 @@ Copyright 2005-2015 Automattic, Inc.
 if (!defined('ABSPATH')) {
     exit;
 }
+
+function create_custom_post_type() {
+    $args = array(
+        'label'                 => __( 'Movies', 'myplugin' ), // Human-readable name of the post type.
+        'description'           => __( 'Custom post type for movies', 'myplugin' ), // Description of the post type.
+        'labels'                => array(
+            'name'          => __( 'Movies', 'myplugin' ),
+            'singular_name' => __( 'Movie', 'myplugin' ),
+            'add_new'       => __( 'Add New', 'myplugin' ),
+            'add_new_item'  => __( 'Add New Movie', 'myplugin' ),
+            'edit_item'     => __( 'Edit Movie', 'myplugin' ),
+            'new_item'      => __( 'New Movie', 'myplugin' ),
+            'all_items'     => __( 'All Movies', 'myplugin' ),
+            'view_item'     => __( 'View Movie', 'myplugin' ),
+            'search_items'  => __( 'Search Movies', 'myplugin' ),
+            'not_found'     => __( 'No movies found', 'myplugin' ),
+            'not_found_in_trash' => __( 'No movies found in Trash', 'myplugin' ),
+            'parent_item_colon' => '',
+            'menu_name'     => __( 'Movies', 'myplugin' ),
+        ),
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'query_var'             => true,
+        'rewrite'               => array( 'slug' => 'movies' ),
+        'capability_type'       => 'post',
+        'has_archive'           => true,
+        'hierarchical'          => false,
+        'menu_position'         => null,
+        'supports'              => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments', 'custom-fields' ),
+        'taxonomies'            => array( 'category', 'post_tag' ),
+        'show_in_rest'          => true,
+        'rest_base'             => 'movies',
+        'rest_controller_class' => 'WP_REST_Posts_Controller',
+    );
+    register_post_type( 'movies', $args );
+}
+
+function my_plugin_register_meta() {
+    register_post_meta(
+        'movies',
+        'genre',
+        array(
+            'single'        => true,
+            'type'          => 'string',
+            'auth_callback' => '__return_true',
+            'default'       => 'drama',
+            'show_in_rest'  => true,
+        )
+    );
+}
+
+function my_plugin_test_code() {
+    $meta_keys = get_registered_meta_keys( 'post', 'movies' );
+    var_dump($meta_keys);
+    if ( registered_meta_key_exists( 'post', 'genre', 'movies' ) ) {
+        echo 'The "genre" meta key is registered for the "movies" post type.';
+        die;
+    } else {
+        echo 'The "genre" meta key is not registered for the "movies" post type.';
+        die;
+    }
+}
+add_action( 'init', 'create_custom_post_type' );
+add_action( 'init', 'my_plugin_register_meta' );
+add_action( 'init', 'my_plugin_test_code' );
